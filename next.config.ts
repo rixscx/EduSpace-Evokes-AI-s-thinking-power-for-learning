d819d807161afa1baeb63c20d7f477f8e67371ff
rixscx <livinell73@gmail.com>
2025-06-22 13:21:52 +0530

    Updated features/fixed bugs/added new UI elements

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
-
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // ✅ Required for `next export`
   typescript: {
     ignoreBuildErrors: true,
   },
